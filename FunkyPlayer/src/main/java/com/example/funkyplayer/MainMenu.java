83e725f36deb2cda90a8e12aaca8bff71329b44b
Genjo15 <anthony.chhim@gmail.com>
2013-11-09 15:58:00 +0100

    Initial commit

---
+++ FunkyPlayer/src/main/java/com/example/funkyplayer/MainMenu.java
@@ -0,0 +1,12 @@
+package com.example.funkyplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
