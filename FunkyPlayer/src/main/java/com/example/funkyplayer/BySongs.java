16fd416ea5b18f53fa01845f09e4309894a555e9
Genjo15 <anthony.chhim@gmail.com>
2013-11-11 14:57:51 +0100

    PlaySong: Prepare bundle

--- FunkyPlayer/src/main/java/com/example/funkyplayer/BySongs.java
+++ FunkyPlayer/src/main/java/com/example/funkyplayer/BySongs.java
@@ -1,13 +1,18 @@
 package com.example.funkyplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class BySongs extends Activity
 {
     ListView listViewBySongs;
+    Library trackList;
 
     public void onCreate(Bundle savedInstanceState)
     {
@@ -15,7 +20,7 @@ public class BySongs extends Activity
         setContentView(R.layout.list_view);
 
         // Get Library from intent
-        Library trackList = (Library)getIntent().getSerializableExtra("library");
+        trackList = (Library)getIntent().getSerializableExtra("library");
 
         // Set references to listView
         listViewBySongs = (ListView)findViewById(R.id.listview);
@@ -23,6 +28,30 @@ public class BySongs extends Activity
         // Set ListView adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, trackList.GetSongsName());
         listViewBySongs.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listViewBySongs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String  itemValue = (String) listViewBySongs.getItemAtPosition(position);
+
+                // Prepare bundle to sent
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("library", trackList);
+
+                // Create and prepare intent
+                Intent intent = new Intent(BySongs.this, Launcher.class);
+                intent.putExtras(bundle);
+
+                // Switch activity
+                startActivity(intent);
+            }
+        });
     }
 }
 
---
+++ FunkyPlayer/src/main/java/com/example/funkyplayer/Launcher.java
@@ -0,0 +1,14 @@
+package com.example.funkyplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Launcher extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launcher);
+    }
+}
