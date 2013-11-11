810fef28e110d0bb5defeaeb48b960c98b260955
Genjo15 <anthony.chhim@gmail.com>
2013-11-11 19:52:50 +0100

    PlaySong: Play song

--- FunkyPlayer/src/main/java/com/example/funkyplayer/Launcher.java
+++ FunkyPlayer/src/main/java/com/example/funkyplayer/Launcher.java
@@ -1,10 +1,23 @@
 package com.example.funkyplayer;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.net.URI;
@@ -12,16 +25,32 @@ import java.net.URI;
 
 public class Launcher extends Activity
 {
+    private final Handler handler = new Handler();
+
     private Library trackList;
     private String musicPlayedName;
-    private MediaPlayer mediaPlayer;
-    private Button buttonPlayStop;
+    private static MediaPlayer mediaPlayer;
     private Song song2Play;
+    private Boolean random;
+    private Boolean repeat;
+
+    private ImageButton buttonPlayPause;
+    private ImageButton buttonRandom;
+    private ImageButton buttonRepeat;
+    private ImageView songCoverArt;
+    private TextView textViewSong;
+    private TextView textViewArtist;
+    private TextView textViewAlbum;
+    private SeekBar seekbar;
 
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launcher);
+
+        // Initialize booleans
+        random = false;
+        repeat = false;
 
         // Get Library from intent + selected song name
         Intent intent = getIntent();
@@ -29,20 +58,167 @@ public class Launcher extends Activity
         musicPlayedName = intent.getStringExtra("song_selected");
 
         // Set references to widgets
-        buttonPlayStop = (Button) findViewById(R.id.ButtonPlayStop);
+        buttonPlayPause = (ImageButton) findViewById(R.id.button_play_pause);
+        buttonRandom = (ImageButton)findViewById(R.id.button_random);
+        buttonRepeat = (ImageButton)findViewById(R.id.button_repeat);
+        songCoverArt = (ImageView) findViewById(R.id.album_art);
+        textViewSong = (TextView)findViewById(R.id.song);
+        textViewArtist = (TextView)findViewById(R.id.artist);
+        textViewAlbum = (TextView)findViewById(R.id.album);
+        seekbar = (SeekBar)findViewById(R.id.seekbar);
 
+        // Launch song
+        LaunchSong();
+
+
+        /*******************************************\
+         * Set event handler for play/pause button *
+        \*******************************************/
+
+        buttonPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(mediaPlayer.isPlaying())
+                {
+                    mediaPlayer.pause();
+                    buttonPlayPause.setImageResource(R.drawable.play);
+                }
+
+                else
+                {
+                    mediaPlayer.start();
+                    StartPlayProgressUpdater();
+                    buttonPlayPause.setImageResource(R.drawable.pause);
+                }
+            }
+        });
+
+        /***************************************\
+         * Set event handler for random button *
+        \***************************************/
+
+        buttonRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(!random)
+                {
+                    random = true;
+                    buttonRandom.setImageResource(R.drawable.random_enabled);
+                    Toast.makeText(getApplicationContext(),"Random mode enabled", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    random = false;
+                    buttonRandom.setImageResource(R.drawable.random_disabled);
+                    Toast.makeText(getApplicationContext(),"Random mode disabled", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        /***************************************\
+         * Set event handler for repeat button *
+        \***************************************/
+
+        buttonRepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(!repeat)
+                {
+                    repeat = true;
+                    buttonRepeat.setImageResource(R.drawable.repeat_enabled);
+                    Toast.makeText(getApplicationContext(),"Repeat enabled", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    repeat = false;
+                    buttonRepeat.setImageResource(R.drawable.repeat_disabled);
+                    Toast.makeText(getApplicationContext(),"Repeat disabled", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        /*************************************\
+         * Set event handler for the seekbar *
+        \*************************************/
+
+        seekbar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                SeekBar sb = (SeekBar)view;
+                mediaPlayer.seekTo(sb.getProgress());
+
+                return false;
+            }
+        });
+    }
+
+    /***************\
+     * Launch Song *
+    \***************/
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD_MR1)
+    private void LaunchSong()
+    {
         // Get song to play
         song2Play = trackList.GetSong(musicPlayedName);
 
-        // Show Alert test
-        Toast.makeText(getApplicationContext(),
-                "name :" + song2Play.GetName() + "  path : " + song2Play.GetPath() + "   artist : " + song2Play.GetArtist() + "   album : " + song2Play.GetAlbum(), Toast.LENGTH_LONG)
-                .show();
+        // Create MediaPlayer object (if an instance is running kill it)
+        if (mediaPlayer != null) {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        mediaPlayer = MediaPlayer.create(this, Uri.parse(song2Play.GetPath()));
+        mediaPlayer.start();
+        StartPlayProgressUpdater();
 
-        // Create MediaPlayer object
-       //URI uri = new ur
-       //mediaPlayer = MediaPlayer.create(this, R.raw.test);
+        // Set song name
+        textViewSong.setText(song2Play.GetName());
 
+        // Set artist name
+        textViewArtist.setText(song2Play.GetArtist());
 
+        // Set album name
+        textViewAlbum.setText(song2Play.GetAlbum());
+
+        // Set image art
+        MediaMetadataRetriever MMR= new MediaMetadataRetriever();
+        MMR.setDataSource(song2Play.GetPath());
+        byte[] art;
+        art = MMR.getEmbeddedPicture();
+        Bitmap songImage = BitmapFactory.decodeByteArray(art, 0, art.length);
+        songCoverArt.setImageBitmap(songImage);
+
+        // Set seekbar
+        seekbar.setMax(mediaPlayer.getDuration());
+    }
+
+    /***********************\
+     * Update Progress bar *
+    \***********************/
+
+    public void StartPlayProgressUpdater()
+    {
+        seekbar.setProgress(mediaPlayer.getCurrentPosition());
+
+        if(mediaPlayer.isPlaying())
+        {
+            Runnable notification = new Runnable() {
+                @Override
+                public void run() {
+                    StartPlayProgressUpdater();
+                }
+            };
+            handler.postDelayed(notification, 1000);
+        }
+        else
+        {
+            mediaPlayer.pause();
+            seekbar.setProgress(0);
+        }
     }
 }
--- FunkyPlayer/src/main/java/com/example/funkyplayer/Song.java
+++ FunkyPlayer/src/main/java/com/example/funkyplayer/Song.java
@@ -12,6 +12,7 @@ public class Song implements Serializable
     private String name;
     private String artist;
     private String album;
+    //private byte[] image;
 
     /***************\
      * Constructor *
@@ -29,6 +30,7 @@ public class Song implements Serializable
         name = new String();
         artist = new String();
         album = new String();
+        //image = null;
     }
 
     /*********************\
@@ -43,9 +45,14 @@ public class Song implements Serializable
 
         try
         {
+            byte[] art;
+
             name = MMR.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE).toString();
             artist = MMR.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST).toString();
             album = MMR.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM).toString();
+            //art = MMR.getEmbeddedPicture();
+            //System.arraycopy(art,0,this.image,0,art.length);
+            //Bitmap bitmap = BitmapFactory.decodeByteArray(art,0,art.length);
         }
         catch (Exception e) {
         }
@@ -59,11 +66,16 @@ public class Song implements Serializable
     public String GetPath(){return path;}
     public String GetArtist(){return artist;}
     public String GetAlbum(){return album;}
+    //public byte[] GetImage(){return image;}
 
     public void SetName(String name){this.name = name;}
     public void SetPath(String path){this.path = path;}
     public void SetArtist(String artist){this.artist = artist;}
     public void SetAlbum(String album){this.album = album;}
+    //public void SetImage(byte[] image)
+    //{
+    //    System.arraycopy(image,0,this.image,0,image.length);
+    //}
 
     public void CopyFrom(Song s)
     {
@@ -71,6 +83,7 @@ public class Song implements Serializable
         SetName(s.GetName());
         SetArtist(s.GetArtist());
         SetAlbum(s.GetAlbum());
+        //SetImage(s.GetImage());
     }
 }
 
